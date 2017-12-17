6910de1816062fd4bd05fa50c54fe2849775d436
Mangirdas Skripka <mangirdas.skripka@gmail.com>
2017-12-17 13:57:47 +0200

    nodemon

--- RestMock/server.js
+++ RestMock/server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/restmock', (req, res) => res.send('Hello World!'))
+
+app.all('/*', function (req, res, next) {
+    res.send("Any URL")
+})
 
 app.listen(8081, () => console.log('Example app listening on port 8081!'))
